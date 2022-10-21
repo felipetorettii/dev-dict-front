fed07870b085d7ccaffb7e449ee8b41e89f319eb
Felipe <felipetdemelo@outlook.com>
2022-10-20 21:39:11 -0300

    Loading component

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -5,6 +5,7 @@ import { LogoNoDots } from "./LogoNoDots";
 import { SearchInput } from "./SearchInput";
 import InfiniteScroll from 'react-infinite-scroll-component';
 import loading from '../assets/loading.gif'
+import ReactLoading from 'react-loading';
 import './Result.css'
 
 interface Result {
@@ -17,12 +18,14 @@ export function Result() {
   const { state } = useLocation();
   const [text, setText] = useState(state.text);
   const [results, setResults] = useState<Result[]>(state.results);
+  const [loadingResults, setLoadingResults] = useState(false);
   const [page, setPage] = useState(state.page);
   const initialText = state.text;
 
   const refreshResults = async () => {
     if (text.length === 0) return;
     let newResults : Result[] = [];
+    setLoadingResults(true);
     let newPage = 0;
     while (newResults.length < 20) {
       const response = await doSearchRequest(text, newPage);
@@ -31,6 +34,7 @@ export function Result() {
     }
     setResults(newResults);
     setPage(newPage);
+    setLoadingResults(false);
   }
 
   const getMoreResults = async () => {
@@ -63,33 +67,38 @@ export function Result() {
         </div>
       </div>
       <div className="dashed"></div>
-      <div className="center_col">
-        <ul>
-          <InfiniteScroll
-            dataLength={results.length}
-            next={getMoreResults}
-            hasMore={true}
-            loader={<img className="w-20 h-20" src={loading} alt="loading..."/>}
-            >
-            {results.map((res, index) => {
-              return (
-                <li key={index}>
-                  <cite role="text" className="cite-host">{getHostWithProtocol(res.Link || "")}
-                    <span role="text"> › ...</span>
-                    <a className="anchor-translated" href={getTranslatedPage(res.Link || "")}>Ver página traduzida</a>
-                  </cite>
-                  <br/>
-                  <a className="text-[#1a0dab] hover:underline visited:text-[#681da8] w-fit text-xl" href={res.Link}>
-                    {res.Title}
-                  </a>
-                  <p>{res.Description}</p>
-                  <br></br>
-                </li>
-              )
-            })}
-          </InfiniteScroll>
-        </ul>
-      </div>
+      {loadingResults ? 
+        <div className="loading-component">
+          <ReactLoading type="bubbles" color="#e5e7eb" height="5%" width="5%"/>
+        </div> : 
+        <div className="center_col">
+          <ul>
+            <InfiniteScroll
+              dataLength={results.length}
+              next={getMoreResults}
+              hasMore={true}
+              loader={<img className="w-20 h-20" src={loading} alt="loading..."/>}
+              >
+              {results.map((res, index) => {
+                return (
+                  <li key={index}>
+                    <cite role="text" className="cite-host">{getHostWithProtocol(res.Link || "")}
+                      <span role="text"> › ...</span>
+                      <a className="anchor-translated" href={getTranslatedPage(res.Link || "")}>Ver página traduzida</a>
+                    </cite>
+                    <br/>
+                    <a className="text-[#1a0dab] hover:underline visited:text-[#681da8] w-fit text-xl" href={res.Link}>
+                      {res.Title}
+                    </a>
+                    <p>{res.Description}</p>
+                    <br></br>
+                  </li>
+                )
+              })}
+            </InfiniteScroll>
+          </ul>
+        </div>
+      }
     </div>
   )
 }
