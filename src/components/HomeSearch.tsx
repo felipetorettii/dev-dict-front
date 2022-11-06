7ea95d736b39d97914ec188d7d7492d89ca5c74f
Junkes887 <henriquejunkes887@gmail.com>
2022-11-06 19:16:52 -0300

    correções

--- src/components/HomeSearch.tsx
+++ src/components/HomeSearch.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import doSearchRequest from '../services/SearchService';
 import { SearchInput } from './SearchInput';
-
+import ReactLoading from 'react-loading';
 
 interface Result {
   Description?: string
@@ -12,16 +12,21 @@ interface Result {
 
 export function HomeSearch() {
   const [text, setText] = useState('');
+  const [loadingResults, setLoadingResults] = useState(false);
   const navigate = useNavigate();
 
   const doSearch = async () => {
     if (text.length === 0) return;
+    setLoadingResults(true);
     let results : Result[] = [];
     let page = 0;
     while (results.length < 20) {
       const response = await doSearchRequest(text, page);
-      results = results.concat(response);
       page += 10;
+      if (response == null) {
+        continue;
+      }
+      results = results.concat(response);
     }
     navigate("/results", { state : { results, text, page} });
     
@@ -29,10 +34,17 @@ export function HomeSearch() {
 
   return (
     <>
-      <SearchInput value={text} handleOnChange={setText} doSearchKeyFunction={doSearch}/>
-      <div>
-        <input type="button" onClick={doSearch} className="button-search" value="Pesquisar"/>
-      </div>
+      {loadingResults ? 
+        <div className="loading-component">
+          <ReactLoading type="bubbles" color="#e5e7eb" height="5%" width="5%"/>
+        </div> : 
+        <>
+          <SearchInput value={text} handleOnChange={setText} doSearchKeyFunction={doSearch}/>
+          <div>
+            <input type="button" onClick={doSearch} className="button-search" value="Pesquisar"/>
+          </div>
+        </>
+      }
     </>
   )
   
